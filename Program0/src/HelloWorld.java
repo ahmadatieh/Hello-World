50917051c290111e080c5793180393ff6b4f192e
ahmadatieh <aatieh123@gmail.com>
2016-11-10 03:29:43 -0500

    Add files via upload

---
+++ Program0/src/HelloWorld.java
@@ -0,0 +1,26 @@
+/* Program of Assignment #: Hello World Assignment 0
+
+* Programmer: Ahmad Atieh
+
+* Due Date: 8/28/15
+
+* COMP110-001, Fall 2015     Instructor: Prof. Kris Jordan 
+
+* Pledge: I have neither given nor recieved unauthorized aid on this program
+
+* Description: A program meant to output the phrase "HelloWorld"
+
+* Input: None
+	
+* Output: Hello World
+*/
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World");
+
+	}
+
+}
---
+++ Program0/src/NationalChampions.java
@@ -0,0 +1,19 @@
+
+public class NationalChampions {
+	
+	public static void main(String[] args){
+		int points;
+		
+		// Round 1: UNC scores 101 Points
+		points = 101;
+			System.out.println("First round " + points);
+		// Round 2: UNC scores 84 more points
+		points = points + 84;
+		System.out.println("Second Round " + points);
+		//Round 3: UNC scores 98 more points
+		points= points + 98;
+		System.out.println("Sweet 16 - " + points);
+		System.out.println("UNC makes the Final Four");
+	}
+
+}
